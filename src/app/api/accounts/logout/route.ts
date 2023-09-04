18ba47abd7c1d8b6ef92449dafe081678e262001
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-09-04 19:46:12 +0700

    Fix logout error

--- src/app/api/accounts/logout/route.ts
+++ src/app/api/accounts/logout/route.ts
@@ -1,15 +1,7 @@
 import { NextResponse } from 'next/server'
-import axios from 'axios'
 import { cookies } from 'next/headers'
 
 export async function POST() {
-  const token = cookies().get('token')?.value
-  await axios.post('http://localhost:5000/accounts/signout', null, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-
   cookies().delete('token')
 
   return NextResponse.json({ data: 'ok' })
